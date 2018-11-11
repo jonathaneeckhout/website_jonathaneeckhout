bc7cd31cd61cf5b298aaca5f75bd91ab494ebed4
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2018-11-11 18:54:40 +0100

    Add css styles support

--- backend/server.js
+++ backend/server.js
@@ -7,25 +7,33 @@ var PORT = 4567;
 var WEBPAGEDIR = "./www";
 var HOMEPAGE = WEBPAGEDIR + "/index.html";
 
+var HTTP_HTML_HEADER = {'Content-Type': 'text/html'};
+var HTTP_CSS_HEADER = {'Content-Type': 'text/css'};
+
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
 
     var filename = "";
 
-    console.log("Pathname: " + q.pathname);
     if (q.pathname === '/') {
         filename = HOMEPAGE;
     } else {
         filename = WEBPAGEDIR + q.pathname;
     }
-    console.log("Filename: " + filename);
+
+    var header = HTTP_HTML_HEADER;
+    if (q.pathname.endsWith("css")) {
+        header = HTTP_CSS_HEADER;
+    }
+
     fs.readFile(filename, function(err, data) {
         if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.writeHead(404, header);
             return res.end("404 Not Found");
         }
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, header);
         res.write(data);
         return res.end();
     });
+
 }).listen(PORT);
