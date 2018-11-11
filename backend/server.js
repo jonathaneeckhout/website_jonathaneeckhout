8f2b4decf9670a557e4cbb2a57ad17bf32d912d5
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2018-11-11 18:28:55 +0100

    init commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+var PORT = 4567;
+
+var WEBPAGEDIR = ".";
+var HOMEPAGE = WEBPAGEDIR + "/index.html";
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+
+    var filename = "";
+    if (q.pathname) {
+        filename = HOMEPAGE;
+    } else {
+        filename = WEBPAGEDIR + q.pathname;
+    }
+    console.log(filename);
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(PORT);
