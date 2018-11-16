00c87c7c21921eed389d97aa39e966b24b01269c
jojo <eeckhout.jonathan@gmail.com>
2018-11-16 17:02:49 +0100

    add support of images location

--- backend/server.js
+++ backend/server.js
@@ -5,28 +5,39 @@ var fs = require('fs');
 var PORT = 4567;
 
 var WEBPAGEDIR = "./www";
+var IMGPAGEDIR = "./";
 var HOMEPAGE = WEBPAGEDIR + "/index.html";
 
 var HTTP_HTML_HEADER = {'Content-Type': 'text/html'};
 var HTTP_CSS_HEADER = {'Content-Type': 'text/css'};
 var HTTP_ALL_HEADER = {'Content-Type': '*/*'};
 
+function getHttpHeaderContentType(req) {
+  if (req.headers['accept'] == null) {
+      res.writeHead(200, HTTP_HTML_HEADER);
+      return "error";
+  }
+
+  var contentType =  req.headers['accept'].split(",")[0];
+
+  return contentType;
+
+}
+
 function setHttpHeader(req, res) {
-    if (req.headers['accept'] == null) {
-        res.writeHead(200, HTTP_HTML_HEADER);
-        return;
-    }
-    
-    var contentType =  req.headers['accept'].split(",")[0];
+  contentType = getHttpHeaderContentType(req);
 
     if (contentType === 'text/html') {
         res.writeHead(200, HTTP_HTML_HEADER);
+        return true;
     } else if (contentType === 'text/css') {
         res.writeHead(200, HTTP_CSS_HEADER);
+        return true;
     } else if (contentType === '*/*') {
         res.writeHead(200, HTTP_ALL_HEADER);
+        return true;
     } else {
-        res.writeHead(200, HTTP_HTML_HEADER);
+      return false;
     }
 }
 
@@ -35,13 +46,24 @@ function sendHttpError (req, res) {
     return res.end("404 Not Found");
 }
 
-function getWebPageFile (path) {
+function getWebPageFile (path, req) {
     var filename = "";
 
-    if (path === '/') {
-        filename = HOMEPAGE;
-    } else {
+    contentType = getHttpHeaderContentType(req);
+
+    if (contentType === 'text/html') {
+        if (path === '/') {
+            filename = HOMEPAGE;
+          } else {
+            filename = WEBPAGEDIR + path;
+          }
+    } else if (contentType === 'text/css') {
         filename = WEBPAGEDIR + path;
+
+    } else if (contentType === '*/*') {
+        filename = IMGPAGEDIR + path;
+    } else {
+
     }
     return filename;
 }
@@ -50,9 +72,10 @@ http.createServer(function (req, res) {
 
     if (req.method === 'GET') {
         var q = url.parse(req.url, true);
+	console.log(q.pathname);
 
-        var filename = getWebPageFile(q.pathname);
-
+        var filename = getWebPageFile(q.pathname, req);
+        console.log(filename);
         fs.readFile(filename, function(err, data) {
             if (err) {
                 return sendHttpError(req, res);
