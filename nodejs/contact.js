7f73bc3fb6c1326a0e52ad311ae467db7d0372ed
jojo <eeckhout.jonathan@gmail.com>
2018-11-30 14:58:34 +0100

    add form handling on server side

---
+++ nodejs/contact.js
@@ -0,0 +1,66 @@
+function formPostDataToObj(formData) {
+    var formObj = {
+    };
+    if (formData == null) {
+        return formObj;
+    }
+    var formDataString = formData.toString('utf8');
+
+    formDataString = formDataString.replace("%40","@");
+    formDataString = formDataString.replace("+"," ");
+
+    var fields = formDataString.split("&");
+
+    for (var i = 0; i < fields.length; i++) {
+        var values = fields[i].split('=');
+        if (values && values.length === 2) {
+            formObj[values[0]] = values[1];
+        }
+    }
+
+    return formObj;
+}
+
+function sendFormMail(server, formData) {
+    if (!formData['firstname']) {
+        console.log('Warning: firstname is not filled in');
+        return;
+    }
+    if (!formData['lastname']) {
+        console.log('Warning: lastname is not filled in');
+        return;
+    }
+    if (!formData['email']) {
+        console.log('Warning: email is not filled in');
+        return;
+    }
+    if (!formData['subject']) {
+        console.log('Warning: subject is not filled in');
+        return;
+    }
+
+    text =
+    'Form filled in:\n' +
+    '\n' +
+    'Firstname: ' + formData['firstname'] +
+    '\n' +
+    'Lastname: ' + formData['lastname'] +
+    '\n' +
+    'E-Mail address: ' + formData['email'] +
+    '\n' +
+    'Text:' +
+    '\n' +
+    formData['subject'];
+
+    server.mail.sendMail(server.mail.getUserEMailAddress(), 'Form data', text);
+}
+
+module.exports = {
+    handlePost : function(server, data) {
+        var formData = formPostDataToObj(data);
+        sendFormMail(server, formData);
+        console.log("Called function in contact.js");
+        //This is the page where the site should return to after filling in the form
+        return '/html/contact.html';
+    }
+};
---
+++ nodejs/index.js
@@ -0,0 +1,5 @@
+//Insert here all the post actions you want to support on your site
+exports.contact = require('./contact')
+
+//Allow the action to be handled by the server
+exports.allowedActions = ['contact'];
