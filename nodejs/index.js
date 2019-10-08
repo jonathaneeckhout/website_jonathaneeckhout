e3480ee4f1186a3de33294f2cd50003f5dc72cc8
jonathan eeckhout <jonathan.eeckhout@softathome.com>
2019-10-08 14:05:58 +0200

    remove all old files

--- nodejs/index.js
+++
@@ -1,6 +0,0 @@
-//Insert here all the post actions you want to support on your site
-exports.contact = require('./contact')
-exports.projects = require('./projects')
-
-//Allow the action to be handled by the server
-exports.allowedActions = ['contact', 'projects'];
